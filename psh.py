2fd4a76a50facc888e2de838e33ff87da86109b4
Max Bernstein <max@bernsteinbear.com>
2024-07-05 14:09:48 -0400

    Simplify psh

--- psh.py
+++ psh.py
@@ -1,86 +1,59 @@
 #!/usr/bin/env python3
 
 """psh: a simple shell written in Python"""
-"""by https://danishpraka.sh/posts/shell-in-python/"""
 
+import io
+import mmap
 import os
 import subprocess
+import shlex
+import sys
 
+# https://xiayingp.gitbook.io/build_a_os/os-interfaces/pipes
 
-def execute_command(command):
-    """execute commands and handle piping"""
-    try:
-        if "|" in command:
-            # save for restoring later on
-            s_in, s_out = (0, 0)
-            s_in = os.dup(0)
-            s_out = os.dup(1)
-
-            # first command takes commandut from stdin
-            fdin = os.dup(s_in)
-
-            # iterate over all the commands that are piped
-            for cmd in command.split("|"):
-                # fdin will be stdin if it's the first iteration
-                # and the readable end of the pipe if not.
-                os.dup2(fdin, 0)
-                os.close(fdin)
-
-                # restore stdout if this is the last command
-                if cmd == command.split("|")[-1]:
-                    fdout = os.dup(s_out)
-                else:
-                    fdin, fdout = os.pipe()
-
-                # redirect stdout to pipe
-                os.dup2(fdout, 1)
-                os.close(fdout)
-
-                try:
-                    subprocess.run(cmd.strip().split())
-                except Exception:
-                    print("psh: command not found: {}".format(cmd.strip()))
-
-            # restore stdout and stdin
-            os.dup2(s_in, 0)
-            os.dup2(s_out, 1)
-            os.close(s_in)
-            os.close(s_out)
-        else:
-            subprocess.run(command.split(" "))
-    except Exception:
-        print("psh: command not found: {}".format(command))
-
-
-def psh_cd(path):
-    """convert to absolute path and change directory"""
-    try:
-        os.chdir(os.path.abspath(path))
-    except Exception:
-        print("cd: no such file or directory: {}".format(path))
-
-
-def psh_help():
-    print("""psh: shell implementation in Python.
-          Supports all basic shell commands.""")
+# TODO(max): Define shell variables and add them to environment variables
+# TODO(max): Pass env to Popen
+# TODO(max): Read shell/environment variables
 
 
 def main():
+    toplevel_stdin = sys.stdin
+    toplevel_stdout = sys.stdout
+    toplevel_stderr = sys.stderr
     while True:
         try:
             inp = input("psh> ")
         except EOFError:
             inp = "exit"
-        if inp == "exit":
-            print("Goodbye.")
-            break
-        elif inp[:3] == "cd ":
-            psh_cd(inp[3:])
-        elif inp == "help":
-            psh_help()
-        else:
-            execute_command(inp)
+        pipeline = inp.split("|")
+        stdin = toplevel_stdin
+        stdout = toplevel_stdout
+        stderr = toplevel_stderr
+        for idx, command in enumerate(pipeline):
+            # TODO(max): Figure out how to do I/O pipes and stuff for built-in
+            # commands.
+            tokens = shlex.split(command)
+            # TODO(max): Ask students why cd needs to be a builtin
+            if tokens[0] == "cd":
+                os.chdir(os.path.abspath(tokens[1]))
+                continue
+            # TODO(max): Ask students why it is fine to comment out pwd "builtin"
+            # if tokens == ["pwd"]:
+            #     print(os.getcwd())
+            #     continue
+            if tokens[0] == "banana":
+                print("Peel!")
+                continue
+            if tokens == ["exit"]:
+                print("Goodbye.")
+                return
+            result = subprocess.Popen(
+                tokens, stdin=stdin, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+            )
+            toplevel_stderr.write(result.stderr.read().decode("utf-8"))
+            stdin = result.stdout
+        toplevel_stdout.write(result.stdout.read().decode("utf-8"))
 
 
-if '__main__' == __name__:
+if "__main__" == __name__:
     main()
